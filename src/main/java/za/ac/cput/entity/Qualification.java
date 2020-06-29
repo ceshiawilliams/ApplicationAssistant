9edd16de0fa24ef08ca707526884183321263188
RasputinZA <RasputinZA1357@users.noreply.github.com>
2020-06-29 16:11:04 +0200

    removed some unnecesary code

--- src/main/java/za/ac/cput/entity/Qualification.java
+++ src/main/java/za/ac/cput/entity/Qualification.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package za.ac.cput.entity;
 
 /** Ceshia Williams
@@ -54,60 +53,3 @@ public class Qualification {
         }
     }
 }
-=======
-package za.ac.cput.entity;
-
-/** Ceshia Williams
- *
- *
- */
-public class Qualification {
-    private String numberOfSubjects;
-    private String levelOfQualifications;
-
-    public static void main( String[] args )
-    {
-
-    }
-
-    private Qualification(String numberOfSubjects, String levelOfQualifications) {
-        this.numberOfSubjects = numberOfSubjects;
-        this.levelOfQualifications = levelOfQualifications;
-    }
-
-    public String getNumberOfSubjects() {
-        return numberOfSubjects;
-    }
-
-    public String getLevelOfQualifications() {
-        return levelOfQualifications;
-    }
-
-    @Override
-    public String toString() {
-        return "Qualification{" +
-                "numberOfSubjects='" + numberOfSubjects + '\'' +
-                ", levelOfQualifications='" + levelOfQualifications + '\'' +
-                '}';
-    }
-
-    public static class QualificationBuilder {
-        private String numberOfSubjects;
-        private String levelOfQualifications;
-
-        public QualificationBuilder setNumberOfSubjects(String numberOfSubjects) {
-            this.numberOfSubjects = numberOfSubjects;
-            return this;
-        }
-
-        public QualificationBuilder setLevelOfQualifications(String levelOfQualifications) {
-            this.levelOfQualifications = levelOfQualifications;
-            return this;
-        }
-
-        public Qualification createQualification() {
-            return new Qualification(numberOfSubjects, levelOfQualifications);
-        }
-    }
-}
->>>>>>> upstream/master
