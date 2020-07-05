4640c8174b6eb78d02737ec9ed058ca6eab1c2b8
Armel Nzawou <arml.nzawou@gmail.com>
2020-07-05 16:19:16 +0200

    Factories - "Faculty and Funding" using TDD - Updated

--- src/main/java/za/ac/cput/factory/FundingFactory.java
+++ src/main/java/za/ac/cput/factory/FundingFactory.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class FundingFactory {
 
-    public static Funding getFunding ( ArrayList <Funding> bursaryList){
+    public static Funding createFunding ( ArrayList <Funding> bursaryList){
 
         return new Funding.Builder()
                 .setBursaryList(bursaryList)
