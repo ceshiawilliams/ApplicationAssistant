7edb165cd0fecdb9501e5536820a878300ad3bf5
Armel Nzawou <arml.nzawou@gmail.com>
2020-08-29 17:25:50 +0200

    Factories - "Faculty and Funding" using TDD - Updated

---
+++ src/main/java/za/ac/cput/repository/previousQualification/impl/QualificationRepositoryImpl.java
@@ -0,0 +1,78 @@
+package za.ac.cput.repository.previousQualification.impl;
+
+import za.ac.cput.entity.previousQualification.Qualification;
+import za.ac.cput.repository.previousQualification.QualificationRepository;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class QualificationRepositoryImpl implements QualificationRepository {
+
+    private static QualificationRepository repository = null;
+    private Set<Qualification> qualificationSet;
+
+    private  QualificationRepositoryImpl()
+    {
+        this.qualificationSet = new HashSet<>();
+    }
+
+    public static QualificationRepository getRepository()
+    {
+        if( repository == null)
+        {
+            repository = new QualificationRepositoryImpl();
+        }
+        return repository;
+
+    }
+    @Override
+    public Set<Qualification> getAll () {return this.qualificationSet;}
+
+    @Override
+    public Qualification create (Qualification qualification)
+    {
+        this.qualificationSet.add(qualification);
+        return qualification;
+    }
+
+    @Override
+    public Qualification read(String s) {
+
+        for (Qualification qualification : this.qualificationSet)
+
+        {
+            if (qualification.getLevelOfQualifications().equalsIgnoreCase(s)) {return qualification;}
+        }
+
+        return null;
+    }
+
+    @Override
+    public Qualification update(Qualification qualification) {
+
+        boolean deleteQualification =  delete (qualification.getLevelOfQualifications());
+
+        if (deleteQualification)
+        {
+            this.qualificationSet.add(qualification);
+            return qualification;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s) {
+
+        Qualification qualification = read(s);
+        if (qualification != null)
+        {
+            this.qualificationSet.remove(qualification);
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
---
+++ src/main/java/za/ac/cput/repository/previousQualification/impl/SubjectRepositoryImpl.java
@@ -0,0 +1,71 @@
+package za.ac.cput.repository.previousQualification.impl;
+
+import za.ac.cput.entity.previousQualification.Subject;
+import za.ac.cput.repository.previousQualification.SubjectRepository;
+
+import java.util.Set;
+
+public class SubjectRepositoryImpl implements SubjectRepository {
+
+    private static SubjectRepository repository = null;
+    private Set<Subject>subjectSet;
+
+    private static SubjectRepository getRepository()
+    {
+        if (repository==null)
+        {
+            repository = new SubjectRepositoryImpl();
+        }
+        return repository;
+    }
+
+    @Override
+    public Set<Subject> getAll() {
+        return this.subjectSet;
+    }
+
+    @Override
+    public Subject create(Subject subject) {
+       this.subjectSet.add(subject);
+       return subject;
+    }
+
+    @Override
+    public Subject read(String s)
+    {
+        for (Subject subject: this.subjectSet)
+        {
+            if (subject.getSubjectName().equalsIgnoreCase(s))
+            {
+                return subject;
+            }
+        }
+        return  null;
+    }
+
+    @Override
+    public Subject update(Subject subject) {
+
+        boolean deleteSubject = delete(subject.getSubjectName());
+
+        if(deleteSubject)
+        {
+            this.subjectSet.add(subject);
+            return subject;
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s) {
+        Subject  subject = read(s);
+        if(subject!=null)
+        {
+            this.subjectSet.remove(subject);
+            return true;
+        }
+
+        return false;
+    }
+}
