fb8bd7c7753976880fdf662fe012cb60574eab52
RasputinZA <RasputinZA1357@users.noreply.github.com>
2020-08-30 21:15:30 +0200

    Created FundingRepository interface, FundingRepositoryImpl and FundingRepositoryImplTest
    I was assigned to create repos for bursary and funding class but after discussion with group members I deleted the bursary class and modified the funding class, FundingFactory, all tests and related references to include the deleted bursary class's attributes and methods.

--- src/main/java/za/ac/cput/entity/financialAid/Funding.java
+++ src/main/java/za/ac/cput/entity/financialAid/Funding.java
@@ -1,40 +1,49 @@
 package za.ac.cput.entity.financialAid;
 
-import java.util.ArrayList;
-
 public class Funding {
-    private ArrayList<Bursary> bursaryList = new ArrayList();
+    private String fundingName;
+    private String fundingRequirements;
 
-    private Funding (Builder builder){
-        this.bursaryList = builder.bursaryList;
+    private Funding(Builder builder){
+        this.fundingName = builder.fundingName;
+        this.fundingRequirements = builder.fundingRequirements;
     }
 
-    public ArrayList<Bursary> getBursaryList() {
-        return bursaryList;
-    }
+    public String getFundingName() { return fundingName;    }
+
+    public String getFundingRequirements() { return fundingRequirements; }
 
     @Override
     public String toString() {
         return "Funding{" +
-                "bursaryList=" + bursaryList +
+                "fundingName='" + fundingName + '\'' +
+                ", fundingRequirements='" + fundingRequirements + '\'' +
                 '}';
     }
 
-    public static class Builder {
-        private ArrayList<Bursary> bursaryList = new ArrayList();
+    public static class Builder{
+        private String fundingName;
+        private String fundingRequirements;
 
-        public Builder setBursaryList(ArrayList<Bursary> bursaryList) {
-            this.bursaryList = bursaryList;
+        public Builder setFundingName(String fundingName) {
+            this.fundingName = fundingName;
+            return this;
+        }
+
+        public Builder setFundingRequirements(String fundingRequirements) {
+            this.fundingRequirements = fundingRequirements;
             return this;
         }
 
         public Builder copy(Funding funding) {
-            this.bursaryList = funding.bursaryList;
+            this.fundingName = funding.fundingName;
+            this.fundingRequirements = funding.fundingRequirements;
             return this;
         }
 
-        public Funding build() {
+        public Funding build(){
             return new Funding(this);
         }
+
     }
 }
