0a9100a18fb3b7493052c05f1c18b5dc44d10e40
abdullahryklief <215245105@mycput.ac.za>
2020-09-05 15:55:24 +0200

    Created the UserService and FacultyService. Created the UserServiceImpl and FacultyServiceImpl. Created UserServiceImplTest and FacultyServiceImplTest.

---
+++ src/main/java/za/ac/cput/service/user/UserService.java
@@ -0,0 +1,12 @@
+package za.ac.cput.service.user;
+
+import za.ac.cput.entity.user.User;
+import za.ac.cput.service.IService;
+
+import java.util.Set;
+
+public interface UserService extends IService<User, String> {
+    Set<User> getAll();
+
+    Set<User> getAllStartingWith(String letter);
+}
---
+++ src/test/java/za/ac/cput/service/tertiaryInstitution/impl/FacultyServiceImplTest.java
@@ -0,0 +1,63 @@
+package za.ac.cput.service.tertiaryInstitution.impl;
+
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import za.ac.cput.entity.tertiaryInstitution.Faculty;
+import za.ac.cput.entity.user.User;
+import za.ac.cput.factory.tertiaryInstitution.FacultyFactory;
+import za.ac.cput.factory.user.UserFactory;
+import za.ac.cput.service.tertiaryInstitution.FacultyService;
+import za.ac.cput.service.user.UserService;
+import za.ac.cput.service.user.impl.UserServiceImpl;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class FacultyServiceImplTest {
+
+    private static FacultyService service = FacultyServiceImpl.getService();
+    private static Faculty faculty = FacultyFactory.createFaculty("ICT123", "Information Technology", "Pass mark 60%");
+
+    @Test
+    public void d_getAll() {
+        Set<Faculty> faculties = service.getAll();
+        Assert.assertEquals(1, faculties.size());
+        System.out.println("Get All: " + faculties);
+    }
+
+    @Test
+    public void e_getAllStartingWith() {
+        Set<Faculty> faculties = service.getAllStartingWith("i");
+        Assert.assertEquals(1, faculties.size());
+        System.out.println("Get All Starting With 'A': " + faculties);
+    }
+
+    @Test
+    public void a_create() {
+        Faculty created = service.create(faculty);
+        Assert.assertEquals(faculty.getFacultyId(),created.getFacultyId());
+        System.out.println("Created: " + created);
+    }
+
+    @Test
+    public void b_read() {
+        Faculty read = service.read(faculty.getFacultyId());
+        System.out.println("Read: " + read);
+    }
+
+    @Test
+    public void c_update() {
+        Faculty updated = new Faculty.Builder().copy(faculty).setFacultyName("Information Computer Technology").build();
+        updated = service.update(updated);
+        System.out.println("Updated: " + updated);
+    }
+
+    @Test
+    public void f_delete() {
+        boolean deleted = service.delete(faculty.getFacultyId());
+        Assert.assertTrue(deleted);
+    }
+}
