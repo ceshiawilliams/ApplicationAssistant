82b91ec763e5a9a1c9f9da2b89fe35f4d23cb4b3
ceshia <ceshiaw21@gmail.com>
2020-09-06 22:22:06 +0200

    Created FundingService, FundingServiceImpl, FundingServiceImplTest and SubjectService, SubjectServiceImpl, SubjectServiceImplTest

---
+++ src/main/java/za/ac/cput/service/financialAid/FundingService.java
@@ -0,0 +1,13 @@
+package za.ac.cput.service.financialAid;
+
+import za.ac.cput.entity.financialAid.Funding;
+import za.ac.cput.service.IService;
+
+import java.util.*;
+
+public interface FundingService extends IService<Funding, String> {
+
+    Set<Funding> getAll();
+
+    Set<Funding> getAllStartingWith(String letter);
+}
